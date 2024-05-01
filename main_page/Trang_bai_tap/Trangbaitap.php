315d4b9b3a17f52c30e77d542f6f38874e608007
tdt8102 <tadt515@gmail.com>
2024-05-01 16:51:26 +0700

    update sau khi lam bai ktra se  hien diem

--- main_page/Trang_bai_tap/Trangbaitap.php
+++ main_page/Trang_bai_tap/Trangbaitap.php
@@ -78,8 +78,15 @@
                 <button type="button" class="btn btn-warning" id="btnFinish">Kết thúc bài kiểm tra</button>
               </div>
           </div>
+          <div class="row">
+              <div class="col-sm-12 text-center">
+                <h4 id="mark"></h4>
+              </div>
+          </div>
         </div>
       </div>
+
+
     </div>
   </div>
 </body>
@@ -90,6 +97,8 @@
     $('#btnFinish').hide();
   });
 
+  let questions;
+  
   $('#btnStart').click(function(){
     GetQuestions();
     $('#btnFinish').show();
@@ -104,16 +113,79 @@
   });
 
   function CheckResult(){
-    alert(1);
+    let mark = 0;
+    $('#questions div.row').each(function(k,v){
+      //buoc 1: lay dap an dung cua cau hoi
+      let id = $(v).find('h5').attr('id');
+
+      // let id = $(v).attr('id');// lay thuoc tinh id cua the h5 = cau hoi => lay id cau hoi
+      let question = questions.find(x=>x.id_quest == id);// tim cau hoi trong mang question dua vao id da co o tren
+      let answer = question['answer'];//lay dap an dung cua cau hoi
+      // console.log(answer);
+
+      //buoc 2: lay dap an cua nguoi dung - radio duoc check
+      let choice = $(v).find('fieldset input[type="radio"]:checked').attr('class');
+      // let choice = '';
+      // switch (da){
+      //   case 'rdOptionA':
+      //     choice = 'A'
+      //   break;
+      //   case 'rdOptionB':
+      //     choice = 'B'
+      //   break;
+      //   case 'rdOptionC':
+      //     choice = 'C'
+      //   break;
+      //   case 'rdOptionD':
+      //     choice = 'D '
+      //   break;  
+      // }
+      if(choice == answer){
+        // console.log('Câu có id: '+id+' Đúng');
+        mark +=2;//moi cau dung dc cong 2 diem
+      }else{
+        console.log('Câu có id: '+id+' Sai');
+      }
+    });
+    console.log('Điểm của bạn là ' +mark);
+    $('#mark').text('Điểm của bạn là ' +mark);
   }
 
   function GetQuestions(){
-    $.ajax({
-      url:'questions.php',
-      type:'get',
-      success:function(data){
-        $('#questions').html(data);
-      }
-    });
-  }
+  $.ajax({
+    url:'questions.php',
+    type:'get',
+    success:function(data){
+      questions = jQuery.parseJSON(data); 
+      let index = 1;
+      let d = '';
+
+      $.each(questions,function(k,v){
+        d+=  '<div class="row" style="display: flex; flex-direction: column; margin-left:10px;">';
+            d+=   '<h5 style="font-weight:bold;" id="'+v['id_quest']+'"><span class="text-danger">Câu '+index+': </span>'+v['question']+'</h5>';
+            
+            d+=  '<fieldset id="group'+index+' ">';
+            d+=   '<div class="radio col-md-12">';
+            d+=       '<label><input type="radio" class="A" name="group'+index+' "><span>A: </span>'+v['option_a']+'</label>';
+            d+=   '</div>';
+
+            d+=   '<div class="radio col-md-12">';
+            d+=       '<label><input type="radio" class="B" name="group'+index+' "><span>B: </span>'+v['option_b']+'</label>';
+            d+=   '</div>';
+
+            d+=   '<div class="radio col-md-12">';
+            d+=       '<label><input type="radio" class="C" name="group'+index+' "><span>C: </span>'+v['option_c']+'</label>';
+            d+=   '</div>';
+
+            d+=   '<div class="radio col-md-12">';
+            d+=       '<label><input type="radio" class="D" name="group'+index+' "><span>D: </span>'+v['option_d']+'</label>';
+            d+=   '</div>';
+            d+=  '</fieldset>';
+        d+=  '</div>';
+        index++;
+      });
+      $('#questions').html(d);
+    }
+  });
+}
 </script>
--- main_page/Trang_bai_tap/questions.php
+++ main_page/Trang_bai_tap/questions.php
@@ -6,33 +6,38 @@
             LIMIT 5";
 
     $result = $connection->query($sql); // Execute the query
-    $index = 1;
-    $data = '';
-    while ($row = $result->fetch_assoc()) {
-        $data.=  '<div class="row" style="display: flex; flex-direction: column; margin-left:10px;">';
-            $data.=   '<h5 style="font-weight:bold;"><span class="text-danger">Câu '.$index++.': </span>'.$row['question'].'</h5>';
+    // $index = 1;
+    // $data = '';
+    // while ($row = $result->fetch_assoc()) {
+    //     $data.=  '<div class="row" style="display: flex; flex-direction: column; margin-left:10px;">';
+    //         $data.=   '<h5 style="font-weight:bold;"><span class="text-danger">Câu '.$index.': </span>'.$row['question'].'</h5>';
             
-            $data.=  '<fieldset id="group'.$index.' ">';
-            $data.=   '<div class="radio col-md-12">';
-            $data.=       '<label><input type="radio" class="rdOptionA" name="group'.$index.' "><span>A: </span>'.$row['option_a'].'</label>';
-            $data.=   '</div>';
+    //         $data.=  '<fieldset id="group'.$index.' ">';
+    //         $data.=   '<div class="radio col-md-12">';
+    //         $data.=       '<label><input type="radio" class="rdOptionA" name="group'.$index.' "><span>A: </span>'.$row['option_a'].'</label>';
+    //         $data.=   '</div>';
 
-            $data.=   '<div class="radio col-md-12">';
-            $data.=       '<label><input type="radio" class="rdOptionB" name="group'.$index.' "><span>B: </span>'.$row['option_b'].'</label>';
-            $data.=   '</div>';
+    //         $data.=   '<div class="radio col-md-12">';
+    //         $data.=       '<label><input type="radio" class="rdOptionB" name="group'.$index.' "><span>B: </span>'.$row['option_b'].'</label>';
+    //         $data.=   '</div>';
 
-            $data.=   '<div class="radio col-md-12">';
-            $data.=       '<label><input type="radio" class="rdOptionC" name="group'.$index.' "><span>C: </span>'.$row['option_c'].'</label>';
-            $data.=   '</div>';
+    //         $data.=   '<div class="radio col-md-12">';
+    //         $data.=       '<label><input type="radio" class="rdOptionC" name="group'.$index.' "><span>C: </span>'.$row['option_c'].'</label>';
+    //         $data.=   '</div>';
 
-            $data.=   '<div class="radio col-md-12">';
-            $data.=       '<label><input type="radio" class="rdOptionD" name="group'.$index.' "><span>D: </span>'.$row['option_d'].'</label>';
-            $data.=   '</div>';
-            $data.=  '</fieldset>';
-            $data.=  '<input type="hidden" name="answer" value=" '.$row['answer'].' ">';
-        $data.=  '</div>';
-        $index++;   
-    }
+    //         $data.=   '<div class="radio col-md-12">';
+    //         $data.=       '<label><input type="radio" class="rdOptionD" name="group'.$index.' "><span>D: </span>'.$row['option_d'].'</label>';
+    //         $data.=   '</div>';
+    //         $data.=  '</fieldset>';
+    //         $data.=  '<input type="hidden" name="answer" value=" '.$row['answer'].' ">';
+    //     $data.=  '</div>';
+    //     $index++;   
+    // }
+    // echo $data;
+    $rows = array(); // Mảng lưu trữ các hàng
 
-    echo $data;
+    while ($row = $result->fetch_assoc()) {
+    $rows[] = $row; // Thêm hàng vào mảng
+}
+    echo json_encode($rows, JSON_UNESCAPED_UNICODE);
 ?>
